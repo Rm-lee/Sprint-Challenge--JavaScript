916ff6778b3494e79d6ae63500b4ee2cdf6623c6
Roger Lee <rmlee0003@gmail.com>
2019-04-28 19:13:18 -0400

    add loop to add universitys to array, add loop to add contactInfo to array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -79,7 +79,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let i =0; i< graduates.length -1; i++){
+  universities.push(graduates[i].university); 
+  universities.sort();
+}
+console.log(JSON.stringify(universities));
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -88,7 +92,10 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo);
+for (let i = 0; i < graduates.length -1; i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
+console.log(JSON.stringify(contactInfo));
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
